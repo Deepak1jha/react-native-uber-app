71212cbadb1435fdd385f7ccf72bc7073dd22ff9
Deepak1jha <ddjha171@gmail.com>
2021-08-22 19:11:22 +0530

    feat : Screen navigation implemented

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/home/HomeScreen";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from "./screens/map/MapScreen";
+
 export default function App(){
+    const Stack = createNativeStackNavigator();
+
     return (
         <Provider store={ store }>
-                <SafeAreaProvider>
-                    <HomeScreen/>
-                </SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="HomeScreen" component={ HomeScreen } options={ { headerShown : false } }/>
+                    <Stack.Screen name="MapScreen" component={ MapScreen } options={ { headerShown : false } }/>
+                </Stack.Navigator>
+            </NavigationContainer>
         </Provider>
 
     );
